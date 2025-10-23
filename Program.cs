2699cba607f6df57f699c3edb007d33a314856d7
Murilo <murilobarcelos130@gmail.com>
2025-10-23 17:44:15 -0300

    alterações parametros explicitos

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
-Nokia nokia = new Nokia("1234-5678", "3310", "123456789012345", 16);
+Nokia nokia = new Nokia(numero: "1234-5678", modelo: "3310", imei: "123456789012345", memoria: 16);
 nokia.Ligar();
 nokia.ReceberLigacao();
 nokia.InstalarAplicativo("WhatsApp");
 
 
-Iphone iphone = new Iphone("9876-5432", "iPhone 13", "987654321098765", 128);
+Iphone iphone = new Iphone(numero: "8765-4321", modelo: "iPhone 13", imei: "543210987654321", memoria: 128);
 iphone.Ligar();
 iphone.ReceberLigacao();
 iphone.InstalarAplicativo("Instagram");
